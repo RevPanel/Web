3424a95bf40a884f5f414d2f44633c2b624842b0
Lorenzo <hello@lorenzo0111.me>
2024-09-09 12:51:12 +0200

    fix: :bug: Fixed bundler

--- next.config.js
+++ next.config.js
@@ -25,6 +25,10 @@ const nextConfig = {
     ],
     dangerouslyAllowSVG: true,
   },
+  webpack: (config) => {
+    config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
+    return config;
+  },
   experimental: {
     serverComponentsExternalPackages: ["@node-rs/argon2"],
   },
